d3186c61d10428f04e77d205807f8f99e3b9a226
Emil Toth <69359704+Fantemil@users.noreply.github.com>
2022-01-12 20:08:04 +0100

    added the option to calculate the velocity

--- PythonMathSolver.py
+++ PythonMathSolver.py
@@ -2,6 +2,7 @@ import time
 import os
 import sys
 import subprocess
+import math
 MacOS = "1"
 Windows= "2"
 
@@ -36,7 +37,7 @@ if Problem == Physics:
     print ("---KINETIC ENERGY---" + "\n The Kinetic Energy (1) The Velocity (2) The Mass (3)")
     InPhysic = input(">>> ")
 clear_console()
-if InPhysic == Kinetic:
+if InPhysic == 1:
     print ("Please tell me the Mass of your Object")
     print(" ")
     Mass = input("In Kilograms >>> ")
@@ -75,3 +76,42 @@ if InPhysic == Kinetic:
     print("The Formula used was the standard formula for calculating the Kinetic Energy in physics. also known as 0,5 ⋅ mv²")
     print("-------------")
     print("♥ Thank you for using PythonMathSolver made with Love by Emil Toth ♥")
+
+
+if InPhysic == "2":
+    clear_console()
+    print("Please Tell me the Mass of your Object (In Kilograms)")
+    Mass = input(">>> ")
+    print("Please Tell me the Kinetic Energy of your Object (In Joule)")
+    KineticEnergy = input(">>> ")
+    clear_console()
+    print("Your Problem is beeing solved in 2 seconds!")
+    time.sleep(1)
+    clear_console()
+    print("Your Problem is beeing solved in 1 second!")
+    time.sleep(1)
+    clear_console()
+    PreResult = 2 * float(KineticEnergy) / float(Mass)
+    Result = math.sqrt(PreResult)
+    print(str(Result) + " are your m/s")
+    kmh = Result * 3.6
+    print(str(kmh) + " are your km/h")
+    print("A SUMMED UP RESULT OF YOUR CALCULATION IS LOADING!")
+    time.sleep(3)
+    clear_console()
+    print("Here is a Result of your Calculation!")
+    print("")
+    print("You were calculating the Velocity with your Kinetic Energy and Mass of your Object")
+    print("")
+    print("Your Kinetic Energy was " + str(KineticEnergy) + " Joule")
+    print("")
+    print("Your Mass was " + str(Mass) + " kg")
+    print("")
+    print("Your Final Result, the Velocity, was " + str(Result) + " m/s  or " + str(kmh) + " km/h")
+    print("")
+    print("")
+    print("The Formula used was the standard formula to calculate the Velocity from the Kinetic Energy and the Mass in Physics also known as 2 ⋅ Ekin : m = Preresult  √Preresult = Endresult")
+    print("-------------")
+    print("♥ Thank you for using PythonMathSolver made with Love by Emil Toth ♥")
+
+
